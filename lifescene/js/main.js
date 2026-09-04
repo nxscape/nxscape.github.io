5247f9d33ebe8a601bf4bf879288a62febaa4f5e
nxscape <167759515+nxscape@users.noreply.github.com>
2026-09-04 23:26:28 +0900

    APPSTORE URL修正

--- lifescene/js/main.js
+++ lifescene/js/main.js
@@ -1,6 +1,5 @@
 // App Store URL を一箇所で差し替えできるようにする
-// TODO: 正しい App Store リンクに差し替えてください
-const APP_STORE_URL = "https://apps.apple.com/jp/app/lifescene/id0000000000";
+const APP_STORE_URL = "https://apps.apple.com/jp/app/lifescene/id6797363961";
 
 for (const a of document.querySelectorAll("[data-appstore-link]")) {
   a.href = APP_STORE_URL;
