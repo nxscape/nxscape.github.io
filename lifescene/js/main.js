d8790898bd6f2b17cdec065ccaa76c234bd756cf
nxscape <167759515+nxscape@users.noreply.github.com>
2026-09-05 00:46:43 +0900

    Googleアナリティクス追加

--- lifescene/js/main.js
+++ lifescene/js/main.js
@@ -3,5 +3,13 @@ const APP_STORE_URL = "https://apps.apple.com/jp/app/lifescene/id6797363961";
 
 for (const a of document.querySelectorAll("[data-appstore-link]")) {
   a.href = APP_STORE_URL;
-}
 
+  a.addEventListener("click", () => {
+    const location = a.getAttribute("data-appstore-location") || "unknown";
+    if (typeof gtag === "function") {
+      gtag("event", "app_store_click", {
+        location: location,
+      });
+    }
+  });
+}
