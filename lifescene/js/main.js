892e36ed7d9dd1dbc77b3f1e5a5ed60315f8e386
nxscape <167759515+nxscape@users.noreply.github.com>
2026-09-15 23:44:46 +0900

    en add

--- lifescene/js/main.js
+++ lifescene/js/main.js
@@ -1,8 +1,15 @@
 // App Store URL を一箇所で差し替えできるようにする
-const isFrenchPage = /\/lifescene\/fr(\/|$)/.test(window.location.pathname);
+const path = window.location.pathname;
+const isFrenchPage = /\/lifescene\/fr(\/|$)/.test(path);
+const isEnglishPage = /\/lifescene\/en(\/|$)/.test(path);
+
 const APP_STORE_URL = isFrenchPage
   ? "https://apps.apple.com/fr/app/lifescene/id6797363961"
-  : "https://apps.apple.com/jp/app/lifescene/id6797363961";
+  : isEnglishPage
+    ? "https://apps.apple.com/us/app/lifescene/id6797363961"
+    : "https://apps.apple.com/jp/app/lifescene/id6797363961";
+
+const localeTag = isFrenchPage ? "fr" : isEnglishPage ? "en" : "ja";
 
 for (const a of document.querySelectorAll("[data-appstore-link]")) {
   a.href = APP_STORE_URL;
@@ -12,7 +19,7 @@ for (const a of document.querySelectorAll("[data-appstore-link]")) {
     if (typeof gtag === "function") {
       gtag("event", "app_store_click", {
         location: location,
-        locale: isFrenchPage ? "fr" : "ja",
+        locale: localeTag,
       });
     }
   });
